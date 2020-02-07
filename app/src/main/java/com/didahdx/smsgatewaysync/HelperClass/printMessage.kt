1a92fcadf86143b54f3f378361a90f46978a132b
Daniel Didah <didahdaniel@gmail.com>
2020-02-07 18:35:00 +0300

    added pdf printer

---
+++ app/src/main/java/com/didahdx/smsgatewaysync/HelperClass/printMessage.kt
@@ -0,0 +1,46 @@
+package com.didahdx.smsgatewaysync.HelperClass
+
+import android.os.Environment
+import com.itextpdf.text.Document
+import com.itextpdf.text.Element
+import com.itextpdf.text.PageSize
+import com.itextpdf.text.pdf.PdfPTable
+import com.itextpdf.text.pdf.PdfWriter
+import java.io.File
+import java.io.FileOutputStream
+import java.util.Date
+
+
+class printMessage {
+    lateinit var pdfFile: File
+
+    fun createPdf(message:String): File{
+        val directoryFolder=File("${Environment.getExternalStorageDirectory()}/SmsGatewaySync}")
+
+        if (!directoryFolder.exists()){
+            directoryFolder.mkdirs()
+        }
+
+        val pdfName= Date().toString()
+        pdfFile=File(directoryFolder.absolutePath,pdfName)
+        val outputStream=FileOutputStream(pdfFile)
+        val document=Document(PageSize.A4)
+        val table= PdfPTable(1)
+        table.defaultCell.horizontalAlignment=Element.ALIGN_CENTER
+        table.defaultCell.fixedHeight=50.0f
+        table.totalWidth=PageSize.A4.width
+        table.widthPercentage=100.0f
+        table.defaultCell.verticalAlignment=Element.ALIGN_MIDDLE
+
+        table.addCell(message)
+
+        PdfWriter.getInstance(document,outputStream)
+        document.open()
+        document.add(table)
+        document.close()
+
+        return pdfFile
+    }
+
+
+}
