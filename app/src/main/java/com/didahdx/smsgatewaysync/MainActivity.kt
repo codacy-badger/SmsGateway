bc5d06f5ba06e444e9c19df90d3475f88125801d
Daniel Didah <didahdaniel@gmail.com>
2020-02-07 18:44:09 +0300

    added permission receive and read sms  permission checks

--- app/src/main/java/com/didahdx/smsgatewaysync/MainActivity.kt
+++ app/src/main/java/com/didahdx/smsgatewaysync/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.didahdx.smsgatewaysync
 
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.FragmentTransaction
 import com.didahdx.smsgatewaysync.ui.HomeFragment
@@ -17,6 +21,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     lateinit var homeFragment: HomeFragment
     lateinit var settingsFragment: SettingsFragment
+    private val PERMISSION_RECEIVE_SMS_CODE = 2
+    private val PERMISSION_READ_SMS_CODE=100
+    private val PERMISSION_WRITE_EXTERNAL_STORAGE_CODE=500
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +44,35 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawerToggle.syncState()
 
         navigation_view.setNavigationItemSelectedListener(this)
+        checkPermission()
 
+        settingUpDefaultFragment()
+    }
+
+    private fun checkPermission(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.RECEIVE_SMS),
+                PERMISSION_RECEIVE_SMS_CODE
+            )
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_SMS),
+                PERMISSION_READ_SMS_CODE
+            )
+        }
+    }
+
+
+    private fun settingUpDefaultFragment() {
         //setting default fragment
         homeFragment = HomeFragment()
         supportFragmentManager
@@ -68,14 +104,43 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
 
         drawer_layout.closeDrawer(GravityCompat.START)
-       return true
+        return true
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_RECEIVE_SMS_CODE -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                   settingUpDefaultFragment()
+                }else{
+                    Toast.makeText(this,"Permission denied",Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+            PERMISSION_READ_SMS_CODE->{
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    settingUpDefaultFragment()
+                }else{
+                    Toast.makeText(this,"Permission denied",Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+
+            PERMISSION_WRITE_EXTERNAL_STORAGE_CODE->{
+                if (grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this,"Permission granted",Toast.LENGTH_LONG).show()
+                }
+
+            }
+        }
+    }
 
     override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)){
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
             drawer_layout.closeDrawer(GravityCompat.START)
-        }else{
+        } else {
             super.onBackPressed()
         }
     }
