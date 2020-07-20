d59e41eec06585a4927c2e7e3f65326a5036c12f
didahdx <didahdaniel@gmail.com>
2020-07-20 17:46:39 +0300

    added runnable for importing sms

--- app/src/main/java/com/didahdx/smsgatewaysync/presentation/smsInbox/SmsInboxViewModel.kt
+++ app/src/main/java/com/didahdx/smsgatewaysync/presentation/smsInbox/SmsInboxViewModel.kt
@@ -5,6 +5,8 @@ import android.app.Application
 import android.database.Cursor
 import android.net.Uri
 import android.os.Debug
+import android.os.Handler
+import android.os.Looper
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -15,6 +17,7 @@ import com.didahdx.smsgatewaysync.domain.SmsInfo
 import com.didahdx.smsgatewaysync.utilities.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import timber.log.Timber
@@ -218,81 +221,103 @@ class SmsInboxViewModel(dataSource: IncomingMessagesDao, application: Applicatio
     }
 
     fun getAllDbSms() {
-        CoroutineScope(IO).launch {
-            val cursor = app.contentResolver?.query(
-                Uri.parse("content://sms/inbox"),
-                null,
-                null,
-                null,
-                null
-            )
-            Timber.i("get all Db message calledv")
-
-            if (cursor != null && cursor.moveToNext()) {
-//                val message_id = cursor.getColumnIndex("_id")
-                val nameId = cursor.getColumnIndex("address")
-                val messageId = cursor.getColumnIndex("body")
-                val dateId = cursor.getColumnIndex("date")
-                val mpesaType = SpUtil.getPreferenceString(app, PREF_MPESA_TYPE, DIRECT_MPESA)
-
-                Timber.i("mpesa sms $mpesaType ")
-                do {
-                    val dateString = cursor.getString(dateId)
-                    val messageBody = cursor.getString(messageId)
-                    val sender = cursor.getString(nameId)
-//                if (cursor.getString(nameId) == "MPESA") {
-
-                    val maskedPhoneNumber = SpUtil.getPreferenceBoolean(app, PREF_MASKED_NUMBER)
-                    val smsFilter = SmsFilter(cursor.getString(messageId), maskedPhoneNumber)
-                    val data = database.getMessage(smsFilter.mpesaId)
-
-                    val message = MpesaMessageInfo(
-                        messageBody,
-                        sdf.format(Date(dateString.toLong())),
-                        sender,
-                        smsFilter.mpesaId,
-                        smsFilter.phoneNumber,
-                        smsFilter.amount,
-                        smsFilter.accountNumber,
-                        smsFilter.name,
-                        dateString.toLong(),
-                        false,
-                        "",
-                        ""
-                    )
-
-                    when (mpesaType) {
-                        PAY_BILL -> {
-                            if (smsFilter.mpesaType == PAY_BILL) {
-                                if (data.isNullOrEmpty()) {
-                                    database.addMessage(message)
-                                }
-                            }
-                        }
-                        DIRECT_MPESA -> {
-                            if (smsFilter.mpesaType == DIRECT_MPESA) {
-                                if (data.isNullOrEmpty()) {
-                                    database.addMessage(message)
-                                }
-                            }
-                        }
-
-                        BUY_GOODS_AND_SERVICES -> {
-                            if (smsFilter.mpesaType == BUY_GOODS_AND_SERVICES) {
-                                if (data.isNullOrEmpty()) {
-                                    database.addMessage(message)
-                                }
-                            }
-                        }
-                    }
-//                }
-
-                } while (cursor.moveToNext())
-                cursor.close()
-                Timber.i("done")
-            }
-        }
+        val importAllMessage=ImportAllDbMessageRunnable(database,app)
+        Thread(importAllMessage).start()
     }
 
+    class ImportAllDbMessageRunnable(dataSource: IncomingMessagesDao, application: Application):Runnable{
+        val app=application
+        val database=dataSource
+        var sdf: SimpleDateFormat = SimpleDateFormat(DATE_FORMAT, Locale.US)
+        override fun run() {
+            val threadHandler = Handler(Looper.getMainLooper())
+            threadHandler.post {
+                app.toast("Importing Messages started")
+            }
+
+            CoroutineScope(IO).launch {
+                val cursor = app.contentResolver?.query(
+                    Uri.parse("content://sms/inbox"),
+                    null,
+                    null,
+                    null,
+                    null
+                )
+                Timber.i("get all Db message ")
+
+                if (cursor != null && cursor.moveToNext()) {
+//                val message_id = cursor.getColumnIndex("_id")
+                    val nameId = cursor.getColumnIndex("address")
+                    val messageId = cursor.getColumnIndex("body")
+                    val dateId = cursor.getColumnIndex("date")
+                    val mpesaType = SpUtil.getPreferenceString(app, PREF_MPESA_TYPE, DIRECT_MPESA)
+
+                    Timber.i("mpesa sms $mpesaType ")
+                    do {
+                        val dateString = cursor.getString(dateId)
+                        val messageBody = cursor.getString(messageId)
+                        val sender = cursor.getString(nameId)
+//                if (cursor.getString(nameId) == "MPESA") {
+
+                        val maskedPhoneNumber = SpUtil.getPreferenceBoolean(app, PREF_MASKED_NUMBER)
+                        val smsFilter = SmsFilter(cursor.getString(messageId), maskedPhoneNumber)
+                        val data = database.getMessage(smsFilter.mpesaId)
+
+                        val message = MpesaMessageInfo(
+                            messageBody,
+                            sdf.format(Date(dateString.toLong())),
+                            sender,
+                            smsFilter.mpesaId,
+                            smsFilter.phoneNumber,
+                            smsFilter.amount,
+                            smsFilter.accountNumber,
+                            smsFilter.name,
+                            dateString.toLong(),
+                            false,
+                            "",
+                            ""
+                        )
+
+                        when (mpesaType) {
+                            PAY_BILL -> {
+                                if (smsFilter.mpesaType == PAY_BILL) {
+                                    if (data.isNullOrEmpty()) {
+                                        database.addMessage(message)
+                                    }
+                                }
+                            }
+                            DIRECT_MPESA -> {
+                                if (smsFilter.mpesaType == DIRECT_MPESA) {
+                                    if (data.isNullOrEmpty()) {
+                                        database.addMessage(message)
+                                    }
+                                }
+                            }
+
+                            BUY_GOODS_AND_SERVICES -> {
+                                if (smsFilter.mpesaType == BUY_GOODS_AND_SERVICES) {
+                                    if (data.isNullOrEmpty()) {
+                                        database.addMessage(message)
+                                    }
+                                }
+                            }
+                        }
+//                }
+
+                    } while (cursor.moveToNext())
+                    cursor.close()
+                    Timber.i("Importing Messages finished")
+                }
+
+                CoroutineScope(Main).launch {
+                    threadHandler.post {
+                        app.toast("Importing Messages finished")
+                    }
+                }
+            }
+
+        }
+
+    }
 
 }
